26b615eefad2d3de9394d69fdb9bdb24b747f34f
284516433@qq.com <284516433@qq.com>
2018-05-28 11:23:32 +0800

    chore: remove element-ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import ElementUI from 'element-ui'
+
 
 import router from './router'
 import app from './app'
 
-Vue.use(ElementUI)
 Vue.use(VueRouter)
 new Vue({
     el: '#app',
